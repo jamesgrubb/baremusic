a54a1acb7233661ddc9617e1590b538a8f6049ca
James <jamesgrubb@mac.com>
2020-07-04 13:06:49 +0100

    geometry loaded

--- src/components/Headphones/index.js
+++ src/components/Headphones/index.js
@@ -1,62 +1,51 @@
-import React, {useRef,useEffect,useState} from 'react'
-import {Canvas, useFrame, useThree} from 'react-three-fiber'
-import { Shadow } from 'drei'
-import {GLTFLoader} from  'three/examples/jsm/loaders/GLTFLoader'
-import Wrapper from '../Wrapper'
-// import useMusicPlayer from '../../hooks/useMusicPlayer'
+import React , {Suspense} from "react";
+import { Canvas, useLoader } from "react-three-fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 
-const Headphones = () => {
-    // const {isPlaying} = useMusicPlayer()
-    const [model,setModel] = useState()
-    useEffect(() =>{
-        new GLTFLoader().load('scene.gltf', setModel)
-    },[])
-    console.log(model)
-    return model ? <primitive object={model.scene} /> : null
-    
+function Loading() {
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        opacity={0.6}
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
+  );
 }
 
-const Scene = () => {
+function ArWing() {
+  const {nodes} = useLoader(GLTFLoader, "scene.gltf");
+// console.log(model.nodes)
+  return (
+    <group position={[0,0,-20]}>
+      <mesh visible geometry={nodes.mesh_0.geometry}>
+        <meshBasicMaterial
+          attach="material"
+          color="red"
+          clearCoatRoughness={2}
+          clearCoat={0.3}
+          flatShading={false}
+          
 
-    
- 
-    const mesh = useRef()
-    const { clock } = useThree()
-    useFrame(() => {
-        mesh.current.position.y = Math.sin(clock.getElapsedTime())
-    })
-    return(
-        <>
-        {/* <ambientLight /> */}
-        {/* <pointLight position={[0, 20, 10]} intensity={5} color="white"/> */}
-        {/* <directionalLight
-      castShadow
-      position={[2.5, 8, 5]}
-      intensity={1.5}
-    /> */}
-        <group ref={mesh} >
-        <mesh rotation={[0,0,0]} position={[0,0,-12]} receiveShadow castShadow>
-            {/* <boxGeometry attach="geometry" args={[2,2,2]}/>                    
-            <meshStandardMaterial attach="material" color="blue" /> */}
-            <Headphones attach="material" color="blue" />
-        </mesh>
-        </group>
-      <Shadow color="#C12020" scale={[4, 4, 1]} opacity={0.2} position={[0, -2, 0]} rotation={[-Math.PI / 2, 0, 0]} />
-        </>
-    )
+        />
+      </mesh>
+    </group>
+  );
 }
 
-export default ()=> {
-    return(
-        <>
-        <Wrapper position="absolute" top="0" right="50%" transform="translateX(50%)" width="20vw" height="20vw" >
-        <Canvas>
-            <Scene />
-            
-        </Canvas>
-        
-        </Wrapper>
-        </>
-    )
+export default function App() {
+  return (
+    <Canvas >
+      {/* <directionalLight intensity={5} /> */}
+      <Suspense fallback={<Loading />}>
+        <ArWing />
+      </Suspense>
+    </Canvas>
+  );
 }
